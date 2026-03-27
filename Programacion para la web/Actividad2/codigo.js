8a1009c76b28a13842e15db4e50f20614a9e37af
Alejandro Olmos <contreras_olmos@outlook.es>
2026-03-27 13:35:39 -0500

    Proyectos Cesde -Trabajos

---
+++ Programacion para la web/Actividad2/codigo.js
@@ -0,0 +1,33 @@
+//DECLARACION VARIABLES
+let tiempo, hr, mn, sg;
+
+//INGRESO DE DATOS
+tiempo = prompt ("Digite la hora (hh:mm:ss)");
+
+//PROCESO
+partes = tiempo.split(":");
+hr = partes[0];
+mn = partes[1];
+sg = partes[2];
+
+//RESULTADO
+if(hr==23 && mn ==59 && sg==59){
+    hr = 0; mn = 0; sg = 0;
+}
+else{
+    if(mn==59 && sg==59){
+        hr+=1; mn=0; sg=0;
+    }
+    else{
+        if(sg=59){
+            m++; sg=0;
+        }
+        else{
+            sg = sg + 1;
+        }
+    }
+}
+
+
+//SALIDA INFORMACION
+document.writeln("La hora, un segundo despues, es " + hr, ":" + mn, ":", + sg);
